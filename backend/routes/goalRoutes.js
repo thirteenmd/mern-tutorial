acd77da4f3e2e5e7aecd687aa703040503715173
thirteenmd <andreacojocaru@gmail.com>
2023-01-16 17:52:17 +0200

    Added middleware

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-router.get('/', (req, res) => {
-  res.status(200).json({message: 'get goals'})
-})
-
-router.post('/', (req, res) => {
-  res.status(200).json({message: 'set goal'})
-})
-
-router.put('/:id', (req, res) => {
-  res.status(200).json({message: `update goal ${req.params.id}`})
-})
-
-router.delete('/:id', (req, res) => {
-  res.status(200).json({message: `delete goal ${req.params.id}`})
-})
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router
