0f419f6ef553f8bdcf664b2ac121be8969d8b6e6
thirteenmd <andreacojocaru@gmail.com>
2023-01-16 16:12:12 +0200

    Added routes and structure

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('hello')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.port || 5000
+
+const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log(`server started on port ${port}`))
