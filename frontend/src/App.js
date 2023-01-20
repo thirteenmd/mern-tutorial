fc485eff9f1bb9d11dac3ef66c61dab283d91574
thirteenmd <andreacojocaru@gmail.com>
2023-01-20 14:17:16 +0200

    Added FE React project

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Regsiter from './pages/Register';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Regsiter />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
