49caa4438c516049a244f040ae29f7ad2d5aebd3
thirteenmd <andreacojocaru@gmail.com>
2023-02-02 13:22:12 +0200

    prepared for heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const path = require('path')
 
 connectDB()
 
@@ -15,6 +16,14 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => res.send('Set to production'))
+}
 
 app.use(errorHandler)
 
